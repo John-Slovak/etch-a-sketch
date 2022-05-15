9c8a2d453d40df1dc5cbac0a2486b2aa76c5ee38
FrankoticSloth <jslovak97@gmail.com>
2022-05-15 14:38:34 +0200

    mobile support

--- script.js
+++ script.js
@@ -1,38 +1,5 @@
-/*const grid = document.querySelector('.grid');
-
-function gridMakeHorizon() {
-    for (i = 1; i <= 16; i++) {
-        let div = document.createElement('div');
-        div.style.cssText = 'background-color: blue; width: 50px; height: 50px;';
-        div.classList.add('h' + i);
-        i.appendChild(div);
-    }
-}
-let numh = 0;
-function gridMakeVert() {
-    for (let i = 1; i <= 16; i++) {
-        let divh = document.createElement('div');
-        numh++;
-        divh.classList.add('b-' + numh + '-' + 1);
-        grid.appendChild(divh);
-        let numv = 1;
-        divh.style.cssText = 'background-color: black; width: 50px; height: 50px;';
-        for (let b = 1; b <= 15; b++) {
-            let divv = document.createElement('div');
-            numv++;
-            divv.classList.add('b-' + numh + '-' + numv);
-            divh.appendChild(divv);
-            divv.style.cssText = 'background-color: black; width: 50px; height: 50px;';
-        }
-    }
-}
-
-gridMakeVert();
-*/
-
-changeSize(16);
-
 let color = 'black';
+let click = true;
 
 function populateBoard(size) {
     let board = document.querySelector('.board');
@@ -40,27 +7,57 @@ function populateBoard(size) {
     squares.forEach((div) => div.remove());
     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    
     let amount = size * size;
     for(let i = 0; i < amount; i++) {
         let square = document.createElement('div');
-        square.addEventListener('mouseover', colorSquare);
+        square.addEventListener('mouseenter', colorSquare);
         square.style.backgroundColor = 'white';
         board.insertAdjacentElement('beforeend', square);
     }
 }
 
+populateBoard(16);
+
 function changeSize(input) {
-    if ((input >= 1) && (input <= 128)) {
+    if ((input >= 1) && (input <= 64)) {
         populateBoard(input);
-    } else {
-        console.log('that is too high');
+    }
+}
+
+function changeOutput(input) {
+    if ((input >= 1) && (input <= 64)) {
+        document.querySelector('.value').textContent = `${input} x ${input}`;
     }
 }
 
 function colorSquare() {
-    this.style.backgroundColor = color;
+    if (click) {
+        if (color === 'rainbow') {
+            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+        } else {
+            this.style.backgroundColor = color;
+        }
+    }
 }
 
 function changeColor(choice) {
     color = choice;
 }
+
+function resetBoard() {
+    let board = document.querySelector('.board');
+    let squares = board.querySelectorAll('div');
+    squares.forEach((div) => div.style.backgroundColor = 'white');
+}
+
+document.querySelector('body').addEventListener('click', (e) => {
+    if (e.target.tagName != 'BUTTON') {
+        click = !click;
+        if (click) {
+            document.querySelector('.mode').textContent = 'Mode: Coloring';
+        } else {
+            document.querySelector('.mode').textContent = 'Mode: Not Coloring'
+        }
+    }
+});
